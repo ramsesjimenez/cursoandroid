bb54c23d10373f12f940595f84685d7fa1699423
Ramses Jimenez <jimenezramses@gmail.com>
2024-06-29 16:05:35 -0400

    primer commit, inicio de proyecto android

---
+++ app/src/main/java/com/example/app01/activity/Alumno.java
@@ -0,0 +1,41 @@
+package com.example.app01.activity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.app01.R;
+
+public class Alumno extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_alumno);
+
+        Button botonok= findViewById(R.id.button);
+
+        botonok.setOnClickListener(v-> {
+            Toast.makeText(this, "Datos Guardado con éxito", Toast.LENGTH_SHORT).show();
+        });
+
+        Button botonsalir= findViewById(R.id.salir);
+
+        botonsalir.setOnClickListener(v-> {
+            finish();
+        });
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
---
+++ app/src/main/java/com/example/app01/activity/Login.java
@@ -0,0 +1,46 @@
+package com.example.app01.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.app01.MainActivity;
+import com.example.app01.R;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login);
+
+        Button buttonok = findViewById(R.id.buttonok);
+
+        buttonok.setOnClickListener(v-> {
+
+            Intent VentanaPrincipal = new Intent(this, Alumno.class);
+
+            startActivity(VentanaPrincipal);
+
+        });
+
+
+
+
+
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
