84a514666fc5bfda34f1c1b3660259f8fed03ed3
Ramses Jimenez <jimenezramses@gmail.com>
2024-06-29 17:56:50 -0400

    primer commit, inicio de proyecto android

--- app/src/main/java/com/example/app01/activity/Login.java
+++ app/src/main/java/com/example/app01/activity/Login.java
@@ -3,6 +3,8 @@ package com.example.app01.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -22,12 +24,24 @@ public class Login extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         Button buttonok = findViewById(R.id.buttonok);
+        EditText txtUsuario = findViewById(R.id.txtUsuario);llll
+        EditText txtPass = findViewById(R.id.txtPass);
+
 
         buttonok.setOnClickListener(v-> {
 
-            Intent VentanaPrincipal = new Intent(this, Alumno.class);
+            if(txtUsuario.getText().toString().isEmpty()) {
+                Toast.makeText(this, "Debe introducir un usuario", Toast.LENGTH_SHORT).show();
+            } else if(txtPass.getText().toString().isEmpty()) {
+                Toast.makeText(this, "Debe introducir un password", Toast.LENGTH_SHORT).show();
 
-            startActivity(VentanaPrincipal);
+            } else {
+                Intent VentanaPrincipal = new Intent(this, Alumno.class);
+
+                startActivity(VentanaPrincipal);
+            }
+
+
 
         });
 
